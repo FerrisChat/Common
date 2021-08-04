5087d8478b4dccfb56dd7d429d029a053b8d46d4
0/0 <zero@imaskeleton.me>
2021-08-04 16:31:31 -0600

    add bad request error type

--- types/errors.rs
+++ types/errors.rs
@@ -2,3 +2,15 @@
 pub struct InternalServerErrorJson {
     pub reason: String,
 }
+
+#[derive(Serialize, Deserialize)]
+pub struct BadRequestJson {
+    pub reason: String,
+    pub location: Option<BadRequestJsonLocation>
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct BadRequestJsonLocation {
+    pub line: u32,
+    pub character: u32
+}
