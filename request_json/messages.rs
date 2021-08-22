ec5c0f6da422eaffefc9e50b1dbfbb234c00a030
0/0 <zero@imaskeleton.me>
2021-08-21 21:31:21 -0600

    add message create JSON

---
+++ request_json/messages.rs
@@ -0,0 +1,6 @@
+/// JSON accepted for POST /api/v0/guilds/{guild_id}/channels/{channel_id}/messages (create message)
+#[derive(Serialize, Deserialize)]
+pub struct MessageCreateJson {
+    /// Message content.
+    pub content: String,
+}
