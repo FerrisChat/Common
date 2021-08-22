813ec6834d7ca285a39085349945fc81a2fd7adb
0/0 <zero@imaskeleton.me>
2021-08-21 18:59:37 -0600

    add get guild URL parameters

--- src/request_json/guilds.rs
+++ src/request_json/guilds.rs
@@ -6,3 +6,16 @@ pub struct GuildCreateJson {
     /// Must be between 1 and 100 Unicode codepoints.
     pub name: String,
 }
+
+/// URL parameters accepted for GET /api/v0/guilds/{guild_id} (get guild)
+pub struct GetGuildUrlParams {
+    /// Return the list of channels in the response?
+    ///
+    /// Defaults to `true` if not specified.
+    channels: Option<bool>,
+
+    /// Return the list of members in the response?
+    ///
+    /// Defaults to `false` if not specified.
+    members: Option<bool>
+}
