b69bb8dfc4d4a7451bfe701541d3031b42a61a4a
hydrostaticcog <hydro@hydrostaticcog.me>
2021-08-22 19:51:15 -0500

    serialize WS

--- src/types/ws.rs
+++ src/types/ws.rs
@@ -1,4 +1,5 @@
 /// JSON returned by GET /api/v0/ws/info.
+#[derive(Serialize, Deserialize)]
 pub struct WsConnectionInfo {
     /// Where to establish a WebSocket connection to.
     pub url: String
