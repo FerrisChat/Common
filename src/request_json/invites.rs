65168a7937e163437fac9eded9652f36856d6838
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-08-27 17:08:23 -0400

    Update invites.rs

--- src/request_json/invites.rs
+++ src/request_json/invites.rs
@@ -2,8 +2,8 @@
 #[derive(Serialize, Deserialize)]
 pub struct InviteCreateJson {
   /// Max age of the invite (Invite will become invalid after this amount of seconds)
-  pub max_age: u32
+  pub max_age: u32,
 
   /// Max uses of the invite (Invite will become invalid after this amount of uses)
-  pub max_uses: u16
+  pub max_uses: u16,
 }
