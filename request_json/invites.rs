27ae7266213f4f7dcfdcc4c31a3be2e09ffce658
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-08-27 17:07:35 -0400

    Create invites.rs

---
+++ request_json/invites.rs
@@ -0,0 +1,9 @@
+/// JSON accepted for POST /api/v0/guilds/{guild_id}/invites/ (create invite)
+#[derive(Serialize, Deserialize)]
+pub struct InviteCreateJson {
+  /// Max age of the invite (Invite will become invalid after this amount of seconds)
+  pub max_age: u32
+
+  /// Max uses of the invite (Invite will become invalid after this amount of uses)
+  pub max_uses: u16
+}
