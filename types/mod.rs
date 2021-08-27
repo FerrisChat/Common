8050857a3ac87fd226cfd6007048203a28fe0276
0/0 <zero@imaskeleton.me>
2021-08-27 09:07:09 -0600

    reexport all message types

--- types/mod.rs
+++ types/mod.rs
@@ -13,7 +13,7 @@ pub use channel::Channel;
 pub use errors::*;
 pub use guild::Guild;
 pub use member::Member;
-pub use message::Message;
+pub use message::*;
 pub use model_type::ModelType;
 pub use user::User;
 pub use ws::WsConnectionInfo;
