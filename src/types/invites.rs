8ec227db9577b89ebcb701cfa7dac757e79c422c
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-08-27 18:32:52 -0400

    Create invites.rs

---
+++ src/types/invites.rs
@@ -0,0 +1,28 @@
+#[derive(Serialize, Deserialize)]
+pub struct Invite {
+    /// The invite's unique code (string)
+    pub code: String,
+
+    /// The invite owner's user ID
+    ///
+    /// 128 bit unsigned integer
+    pub owner_id: u128,
+    
+    /// The invite's guild ID
+    ///
+    /// 128 bit unsigned integer
+    pub guild_id: u128,
+
+    /// When the invite was created as a timezone-naive 
+    pub created_at: chrono::DateTime<chrono::Utc>,
+
+    /// How many times the invite was used
+    pub uses: i32,
+      
+    /// The maximum amount of times the invite can be used before being invalidated
+    pub max_uses: i16,
+    
+    /// The amount of seconds from the time the invite was created until it will expire
+    pub max_age: i64,
+    
+}
