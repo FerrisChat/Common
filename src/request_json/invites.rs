738bf44748cbc0f296b808211203086090552e93
Pumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-08-27 17:14:29 -0400

    Update invites.rs
    Update indentation to match the rest of the files

--- src/request_json/invites.rs
+++ src/request_json/invites.rs
@@ -1,9 +1,9 @@
 /// JSON accepted for POST /api/v0/guilds/{guild_id}/invites/ (create invite)
 #[derive(Serialize, Deserialize)]
 pub struct InviteCreateJson {
-  /// Max age of the invite (Invite will become invalid after this amount of seconds)
-  pub max_age: Option<u32>,
+    /// Max age of the invite (Invite will become invalid after this amount of seconds)
+    pub max_age: Option<u32>,
 
-  /// Max uses of the invite (Invite will become invalid after this amount of uses)
-  pub max_uses: Option<u16>,
+    /// Max uses of the invite (Invite will become invalid after this amount of uses)
+    pub max_uses: Option<u16>,
 }
