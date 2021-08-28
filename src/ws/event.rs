faff025f1e4f0a0d21624cb55705b5ac8a845e6a
cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-08-28 10:21:54 -0700

    Add resume payload to WsEvent

--- src/ws/event.rs
+++ src/ws/event.rs
@@ -1,5 +1,13 @@
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "c", content = "d")]
 pub enum WsEvent {
-    Identify { token: String, intents: u64 },
+    Identify {
+        token: String,
+        intents: u64,
+    },
+    Resume {
+        token: String,
+        session_id: String,
+        sequence: u64,
+    },
 }
