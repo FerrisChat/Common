abc9a09cd2503012f695c48c84df9c5f2550e3a3
Jay3332 <40323796+jay3332@users.noreply.github.com>
2021-09-04 22:16:34 -0400

    Add ``Embed`` model
    Not sure if it belongs in this directory, lmk if it should be moved into another one

---
+++ types/embed.rs
@@ -0,0 +1,30 @@
+#[derive(Serialize, Deserialize)]
+struct EmbedAuthor {
+    pub name: String,
+    pub icon_url: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+struct EmbedFooter {
+    pub text: String,
+    pub icon_url: Option<String>,
+}
+        
+#[derive(Serialize, Deserialize)]
+struct EmbedField {
+    pub name: String,
+    pub value: String,
+    pub inline: bool,
+}
+        
+#[derive(Serialize, Deserialize)]
+pub struct Embed {
+    pub type: String,
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub url: Option<String>,
+    pub timestamp: Option<time::PrimitiveDateTime>,
+    pub author: Option<EmbedAuthor>,
+    pub footer: Option<EmbedFooter>,
+    pub fields: Vec<EmbedField>,
+}
