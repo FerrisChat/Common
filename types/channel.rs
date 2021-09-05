b1f4715d984f9d74bfa1b79a0f4347a8be72eaf1
DistortedPumpkin <70335814+DistortedPumpkin@users.noreply.github.com>
2021-09-05 10:02:14 -0400

    add Role type and request json for creating/updating roles

--- types/channel.rs
+++ types/channel.rs
@@ -5,7 +5,7 @@ pub struct Channel {
     /// 128 bit unsigned integer
     pub id: u128,
 
-    /// The user's name
+    /// The channel's name
     ///
     /// String of max length 100 chars
     pub name: String,
