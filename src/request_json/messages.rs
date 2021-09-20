cd9b263440395d621b90fbcabd0c97a11b02e7f2
0/0 <zero@imaskeleton.me>
2021-09-20 17:06:48 -0600

    add clone impl to all (pt 1)

--- src/request_json/messages.rs
+++ src/request_json/messages.rs
@@ -1,19 +1,19 @@
 /// JSON accepted for POST /api/v0/guilds/{guild_id}/channels/{channel_id}/messages (create message)
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct MessageCreateJson {
     /// Message content.
     pub content: String,
 }
 
 /// PATCH /api/v0/channels/{channel_id}/messages/{message_id}
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct MessageUpdateJson {
     /// Message content.
     pub content: Option<String>,
 }
 
 /// GET /api/v0/channels/{channel_id}/messages
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct GetMessageHistoryParams {
     pub limit: Option<i64>,
 }
