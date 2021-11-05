7295793c95da5ad1b0f67adffb14e2239975708b
0/0 <zero@imaskeleton.me>
2021-11-04 20:28:03 -0600

    add custom Serialize impl for Invite

--- types/invites.rs
+++ types/invites.rs
@@ -1,4 +1,7 @@
-#[derive(Serialize, Deserialize, Clone)]
+use serde::ser::SerializeStruct;
+use serde::{Serialize, Serializer};
+
+#[derive(Deserialize, Clone)]
 pub struct Invite {
     /// The invite's unique code (string)
     pub code: String,
@@ -25,3 +28,28 @@ pub struct Invite {
     /// The amount of seconds from the time the invite was created until it will expire
     pub max_age: Option<i64>,
 }
+
+impl Serialize for Invite {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        let mut self_ser = serializer.serialize_struct("Invite", 9)?;
+
+        self_ser.serialize_field("code", &self.code)?;
+
+        self_ser.serialize_field("owner_id", &self.owner_id)?;
+        self_ser.serialize_field("owner_id_string", &self.owner_id.to_string())?;
+
+        self_ser.serialize_field("guild_id", &self.guild_id)?;
+        self_ser.serialize_field("guild_id_string", &self.guild_id.to_string())?;
+
+        self_ser.serialize_field("created_at", &self.created_at)?;
+        self_ser.serialize_field("uses", &self.uses)?;
+        self_ser.serialize_field("max_uses", &self.max_uses)?;
+        self_ser.serialize_field("max_age", &self.max_age)?;
+
+        self_ser.end()
+    }
+}
+
