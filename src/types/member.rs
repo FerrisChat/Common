2c2ffcb1d6c726a7c2bd39d129a22ed49cfbac3e
0/0 <zero@imaskeleton.me>
2021-11-05 20:33:13 -0600

    add custom Serialize impl for Member

--- src/types/member.rs
+++ src/types/member.rs
@@ -1,6 +1,8 @@
+use serde::{Serialize, Serializer};
+use serde::ser::SerializeStruct;
 use crate::types::{Guild, User};
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Deserialize, Clone)]
 pub struct Member {
     pub user_id: Option<u128>,
     pub user: Option<User>,
@@ -8,3 +10,23 @@ pub struct Member {
     pub guild_id: Option<u128>,
     pub guild: Option<Guild>,
 }
+
+impl Serialize for Member {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+        where
+            S: Serializer,
+    {
+        let mut self_ser = serializer.serialize_struct("Member", 6)?;
+
+        self_ser.serialize_field("user_id", &self.user_id)?;
+        self_ser.serialize_field("user_id_string", &self.user_id.map(|x| x.to_string()))?;
+        self_ser.serialize_field("user", &self.user)?;
+
+        self_ser.serialize_field("guild_id", &self.guild_id)?;
+        self_ser.serialize_field("guild_id_string", &self.guild_id.map(|x| x.to_string()))?;
+        self_ser.serialize_field("guild", &self.guild)?;
+
+        self_ser.end()
+    }
+}
+
