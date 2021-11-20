df1d1e7a68da75fae934a4999513cb118ac167e7
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-20 13:15:38 -0600

    fmt

--- request_json/mod.rs
+++ request_json/mod.rs
@@ -1,15 +1,15 @@
+mod bots;
 mod channels;
 mod guilds;
 mod invites;
 mod messages;
 mod roles;
 mod users;
-mod bots;
 
+pub use bots::*;
 pub use channels::*;
 pub use guilds::*;
 pub use invites::*;
 pub use messages::*;
 pub use roles::*;
 pub use users::*;
-pub use bots::*;
