38c0bcc9958dee0b572475d7b9a37f34dfb10412
hydrostaticcog <hydro@hydrostaticcog.me>
2021-11-20 13:26:27 -0600

    Update bots.rs

--- src/request_json/bots.rs
+++ src/request_json/bots.rs
@@ -11,16 +11,3 @@ pub struct BotUpdateJson {
     /// Bot's username
     pub username: Option<String>,
 }
-
-/// JSON accepted for DELETE /api/v0/users/{user_id}/bots/{bot_id}
-#[derive(Serialize, Deserialize)]
-pub struct BotDeleteJson {
-    /// Always set this to true.
-    pub are_you_sure: bool,
-
-    /// Also always set this to true
-    pub are_you_really_sure: bool,
-
-    /// Just for fun, if you want, set this to true.
-    pub oletko_varma: Option<bool>,
-}
