f03dfb182c1cf8cc2ed0fb238983432605c7411a
valkyrie_pilot <valk@randomairborne.dev>
2021-11-22 11:56:51 -0700

    add more permissions

--- src/perms.rs
+++ src/perms.rs
@@ -12,7 +12,11 @@ bitflags! {
         /// Permissions to add/remove channels.
         const ADD_REMOVE_CHANNELS = 0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_1000;
         /// Permissions to remove members (aka kick them).
-        const REMOVE_MEMBERS =      0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0001_0000;
+        const KICK_MEMBERS =        0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0001_0000;
+        /// Permissions to remove members permanently (aka ban them).
+        const BAN_MEMBERS =         0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0010_0000;
+        /// Permissions to disable a user's ability to talk (aka mute them).
+        const MUTE_MEMBERS =        0b0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0000_0100_0000;
     }
 }
 
