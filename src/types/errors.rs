049a0478964e516e6a2cb016b85d71ece5658c8f
0/0 <zero@imaskeleton.me>
2021-12-01 08:47:55 -0700

    consolidate all error types into one enum

--- src/types/errors.rs
+++ src/types/errors.rs
@@ -1,64 +1,62 @@
-use serde::ser::SerializeStruct;
-use serde::{Serialize, Serializer};
-
 #[derive(Serialize, Deserialize, Clone)]
-pub struct InternalServerErrorJson {
-    pub reason: String,
-    pub is_bug: bool,
-    pub link: Option<String>,
+#[serde(untagged)]
+pub enum ErrorJson {
+    /// JSON returned with a HTTP 500
+    InternalServerError {
+        reason: String,
+        is_bug: bool,
+        link: Option<String>,
+    },
+
+    /// JSON returned with a HTTP 429
+    TooManyRequests {
+        /// This many requests are allowed in `duration` seconds.
+        count: u32,
+        /// `count` requests are allowed in this many seconds.
+        duration: u32,
+        /// You can retry this request in this many seconds.
+        ///
+        /// This number could be the maximum for `i32` (2 to the 31st power minus 1) for some endpoints.
+        /// That will rarely happen (as it stands, only for the Create User endpoint),
+        /// but if it does, signals to just ignore this request and never retry,
+        /// as a lifetime limit has been hit for this IP address.
+        retry_after: i32,
+    },
+
+    /// A catch-all for all other errors.
+    Message { message: String },
 }
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct BadRequestJson {
-    pub reason: String,
-    pub location: Option<BadRequestJsonLocation>,
-}
-
-#[derive(Serialize, Deserialize, Clone)]
-pub struct BadRequestJsonLocation {
-    pub line: u32,
-    pub character: u32,
-}
-
-#[derive(Deserialize, Clone)]
-/// JSON returned along with a HTTP 429 Too Many Requests
-pub struct TooManyRequestsJson {
-    /// This many requests are allowed in `duration` seconds.
-    pub count: u32,
-    /// `count` requests are allowed in this many seconds.
-    pub duration: u32,
-    /// You can retry this request in this many seconds.
-    ///
-    /// This number could be the maximum for `u128` (2 to the 128th power minus 1) for some endpoints.
-    /// That will rarely happen (as it stands, only for the Create User endpoint),
-    /// but if it does, signals to just ignore this request and never retry,
-    /// as a lifetime limit has been hit for this IP address.
-    pub retry_after: u128,
-}
-
-impl Serialize for TooManyRequestsJson {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let mut self_ser = serializer.serialize_struct("TooManyRequestsJson", 4)?;
-
-        self_ser.serialize_field("count", &self.count)?;
-        self_ser.serialize_field("duration", &self.duration)?;
-
-        self_ser.serialize_field("retry_after", &self.retry_after)?;
-        self_ser.serialize_field("retry_after_string", &self.retry_after.to_string())?;
-        self_ser.end()
+impl ErrorJson {
+    /// Create a new Internal Server Error object.
+    #[inline(always)]
+    pub const fn new_500(reason: String, is_bug: bool, link: Option<String>) -> Self {
+        Self::InternalServerError {
+            reason,
+            is_bug,
+            link,
+        }
     }
-}
 
-#[derive(Serialize, Deserialize, Clone)]
-// JSON returned along with HTTP 404 Not Found.
-pub struct NotFoundJson {
-    pub message: String,
-}
+    /// Create a new Bad Request object
+    #[inline(always)]
+    pub const fn new_400(message: String) -> Self {
+        Self::Message { message }
+    }
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Json {
-    pub message: String,
+    /// Create a new Not Found object
+    #[inline(always)]
+    pub const fn new_404(message: String) -> Self {
+        Self::Message { message }
+    }
+
+    /// Create a new Too Many Requests object
+    #[inline(always)]
+    pub const fn new_429(count: u32, duration: u32, retry_after: i32) -> Self {
+        Self::TooManyRequests {
+            count,
+            duration,
+            retry_after,
+        }
+    }
 }
