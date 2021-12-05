0256487162aa2c73c8c1f7a5612cc911896e56a1
0/0 <zero@imaskeleton.me>
2021-12-04 18:06:55 -0700

    add new() to SuccessJson

--- src/types/success.rs
+++ src/types/success.rs
@@ -1,4 +1,14 @@
 #[derive(Deserialize, Serialize, Clone)]
 pub struct SuccessJson {
-    reason: String,
+    pub reason: String,
+}
+
+impl SuccessJson {
+    /// Create a new object of `Self` with the provided reason.
+    ///
+    /// This exists mainly to make the code easier to read.
+    #[inline(always)]
+    pub const fn new(reason: String) -> Self {
+        Self { reason }
+    }
 }
