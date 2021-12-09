7ce3c8fbdb6cc5a20f492176d375b150cce8e205
0/0 <zero@imaskeleton.me>
2021-12-09 08:25:03 -0700

    update permissions implementation to use tribools instead of bitflags

--- src/perms.rs
+++ src/perms.rs
@@ -1,23 +1,14 @@
-use bitflags::bitflags;
-
-bitflags! {
-    #[derive(Default)]
-    pub struct Permissions: i64 {
-        /// Permissions to send messages.
-        const SEND_MESSAGES =       1 << 0;
-        /// Permissions to delete messages.
-        const DELETE_MESSAGES =     1 << 1;
-        /// Permissions to edit channels.
-        const EDIT_CHANNELS =       1 << 2;
-        /// Permissions to add/remove channels.
-        const ADD_REMOVE_CHANNELS = 1 << 3;
-        /// Permissions to remove members (aka kick them).
-        const KICK_MEMBERS =        1 << 4;
-        /// Permissions to remove members permanently (aka ban them).
-        const BAN_MEMBERS =         1 << 5;
-        /// Permissions to disable a user's ability to talk (aka mute them).
-        const MUTE_MEMBERS =        1 << 6;
-    }
+#[derive(
+    Serialize, Deserialize, Default, Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash,
+)]
+pub struct Permissions {
+    pub send_messages: Option<bool>,
+    pub delete_messages: Option<bool>,
+    pub edit_channels: Option<bool>,
+    pub add_remove_channels: Option<bool>,
+    pub kick_members: Option<bool>,
+    pub ban_members: Option<bool>,
+    pub mute_members: Option<bool>,
+    pub add_bots: Option<bool>,
+    pub adminstrator: Option<bool>,
 }
-
-serde_for_bitflags!(Permissions);
