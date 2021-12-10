002ca1beb87852f971eea010c986a434357459cc
0/0 <zero@imaskeleton.me>
2021-12-10 15:23:02 -0700

    switch permissions implementation to `Tribool` instead of `Option<bool>`

--- src/perms.rs
+++ src/perms.rs
@@ -1,61 +1,64 @@
+use std::ops::BitOr;
+use tribool::Tribool;
+
 #[derive(
     Serialize, Deserialize, Default, Debug, Ord, PartialOrd, Eq, PartialEq, Copy, Clone, Hash,
 )]
 pub struct Permissions {
-    pub send_messages: Option<bool>,
-    pub delete_messages: Option<bool>,
-    pub edit_channels: Option<bool>,
-    pub add_remove_channels: Option<bool>,
-    pub kick_members: Option<bool>,
-    pub ban_members: Option<bool>,
-    pub mute_members: Option<bool>,
-    pub add_bots: Option<bool>,
-    pub adminstrator: Option<bool>,
+    pub send_messages: Tribool,
+    pub delete_messages: Tribool,
+    pub edit_channels: Tribool,
+    pub add_remove_channels: Tribool,
+    pub kick_members: Tribool,
+    pub ban_members: Tribool,
+    pub mute_members: Tribool,
+    pub add_bots: Tribool,
+    pub adminstrator: Tribool,
 }
 
 impl Permissions {
     #[inline]
     pub fn all_false() -> Self {
         Self {
-            send_messages: Some(false),
-            delete_messages: Some(false),
-            edit_channels: Some(false),
-            add_remove_channels: Some(false),
-            kick_members: Some(false),
-            ban_members: Some(false),
-            mute_members: Some(false),
-            add_bots: Some(false),
-            adminstrator: Some(false),
+            send_messages: Tribool::False,
+            delete_messages: Tribool::False,
+            edit_channels: Tribool::False,
+            add_remove_channels: Tribool::False,
+            kick_members: Tribool::False,
+            ban_members: Tribool::False,
+            mute_members: Tribool::False,
+            add_bots: Tribool::False,
+            adminstrator: Tribool::False,
         }
     }
 
     #[inline]
     pub fn all_true() -> Self {
         Self {
-            send_messages: Some(true),
-            delete_messages: Some(true),
-            edit_channels: Some(true),
-            add_remove_channels: Some(true),
-            kick_members: Some(true),
-            ban_members: Some(true),
-            mute_members: Some(true),
-            add_bots: Some(true),
-            adminstrator: Some(true),
+            send_messages: Tribool::True,
+            delete_messages: Tribool::True,
+            edit_channels: Tribool::True,
+            add_remove_channels: Tribool::True,
+            kick_members: Tribool::True,
+            ban_members: Tribool::True,
+            mute_members: Tribool::True,
+            add_bots: Tribool::True,
+            adminstrator: Tribool::True,
         }
     }
 
     #[inline]
     pub fn empty() -> Self {
         Self {
-            send_messages: None,
-            delete_messages: None,
-            edit_channels: None,
-            add_remove_channels: None,
-            kick_members: None,
-            ban_members: None,
-            mute_members: None,
-            add_bots: None,
-            adminstrator: None,
+            send_messages: Tribool::Indeterminate,
+            delete_messages: Tribool::Indeterminate,
+            edit_channels: Tribool::Indeterminate,
+            add_remove_channels: Tribool::Indeterminate,
+            kick_members: Tribool::Indeterminate,
+            ban_members: Tribool::Indeterminate,
+            mute_members: Tribool::Indeterminate,
+            add_bots: Tribool::Indeterminate,
+            adminstrator: Tribool::Indeterminate,
         }
     }
 }
