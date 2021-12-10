4fd342571103ec247ed360a114321e82aed66fa3
0/0 <zero@imaskeleton.me>
2021-12-09 21:47:16 -0700

    add permissions implementation quick getters

--- src/perms.rs
+++ src/perms.rs
@@ -12,3 +12,50 @@ pub struct Permissions {
     pub add_bots: Option<bool>,
     pub adminstrator: Option<bool>,
 }
+
+impl Permissions {
+    #[inline]
+    pub fn all_false() -> Self {
+        Self {
+            send_messages: Some(false),
+            delete_messages: Some(false),
+            edit_channels: Some(false),
+            add_remove_channels: Some(false),
+            kick_members: Some(false),
+            ban_members: Some(false),
+            mute_members: Some(false),
+            add_bots: Some(false),
+            adminstrator: Some(false),
+        }
+    }
+
+    #[inline]
+    pub fn all_true() -> Self {
+        Self {
+            send_messages: Some(true),
+            delete_messages: Some(true),
+            edit_channels: Some(true),
+            add_remove_channels: Some(true),
+            kick_members: Some(true),
+            ban_members: Some(true),
+            mute_members: Some(true),
+            add_bots: Some(true),
+            adminstrator: Some(true),
+        }
+    }
+
+    #[inline]
+    pub fn empty() -> Self {
+        Self {
+            send_messages: None,
+            delete_messages: None,
+            edit_channels: None,
+            add_remove_channels: None,
+            kick_members: None,
+            ban_members: None,
+            mute_members: None,
+            add_bots: None,
+            adminstrator: None,
+        }
+    }
+}
