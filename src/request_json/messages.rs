be6d0ecaa247cc6eb16639c649a2cdfba8909082
Cryptex <64497526+Cryptex-github@users.noreply.github.com>
2021-12-16 17:14:56 -0800

    Update messages.rs

--- src/request_json/messages.rs
+++ src/request_json/messages.rs
@@ -1,8 +1,13 @@
-/// JSON accepted for POST `/v0/guilds/{guild_id}/channels/{channel_id}/messages` (create message)
+use crate::types::Embed;
+
+/// JSON accepted for POST `/v0/channels/{channel_id}/messages` (create message)
 #[derive(Serialize, Deserialize, Clone)]
 pub struct MessageCreateJson {
     /// Message content.
     pub content: String,
+    /// Message embed.
+    pub embed: Option<Embed>,
+    /// Message Nonce
     pub nonce: Option<String>,
 }
 
@@ -11,6 +16,8 @@ pub struct MessageCreateJson {
 pub struct MessageUpdateJson {
     /// Message content.
     pub content: Option<String>,
+    /// Message Embed.
+    pub embed: Option<Embed>,
 }
 
 /// GET `/v0/channels/{channel_id}/messages`
