53d0186555674310a25824410c45a876edc42b37
DistortedPumpkin <therealdistortedpumpkin@gmail.com>
2021-12-21 09:39:29 -0500

    Add DMChannel struct

---
+++ src/types/dm_channel.rs
@@ -0,0 +1,17 @@
+#[derive(Deserialize, Clone)]
+pub struct DMChannel {
+    /// The channel ID
+    ///
+    /// 128 bit unsigned integer
+    pub id: u128,
+
+    /// The channel's name
+    /// Only applies to Group DMs
+    /// String of max length 100 chars
+    pub name: Option<String>,
+
+    /// The users inside the channel
+    ///
+    /// Vec of 128 bit unsigned integers
+    pub guild_id: Vec<u128>,
+}
